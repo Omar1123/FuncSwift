1d5ee4381410599abce34a787d6eb5d132da3662
Omar <Omar@Mac-mini-de-Omar.local>
2017-05-23 15:24:27 -0600

    ADD: Numbers

---
+++ Numbers.playground/Contents.swift
@@ -0,0 +1,41 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Numbers
+//Type inference 
+//Is better use this form not the explicit form
+var someNumber = 30
+
+var weight: Int = 200 //Explict declare type int
+
+var milesRan = 56.45
+
+
+var pi: Double = 3.14
+
+pi = milesRan
+
+
+//Arithmetic Operatos
+
+var area = 15 * 20
+var sum = 5 + 6
+var diff = 10 - 3
+var div = 12 / 3
+var div1 = 13 / 5
+
+var remainder = 13 % 5
+
+var result = "The result of 13 / 5 is \(div1) with a remainer of \(remainder)"
+
+
+var randomNumber = 13
+
+if randomNumber % 2 == 0 {
+    print("This is an even number")
+} else {
+    print("This is an odd number")
+}
+
+var result2 = 15 * ((5+7) / 3)
